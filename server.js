0effb53f61dacc49be82f65847308c19bebad467
jiwomdf <mdf.jiwo@gmail.com>
2020-07-20 23:01:29 +0700

    add jwt, still need refresh token

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const bcryipt = require('bcrypt')
 
+const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
 const users = []
+
+const posts = [
+    {
+        username: "jiwo",
+        title: "how to be a good programmer"
+    }
+]
 
 app.get('/users', (req, res) => {
     res.json(users)
@@ -36,7 +46,7 @@ app.post('/users/login', async (req, res) => {
     try {
         const isValid = await bcryipt.compare(req.body.password, user.password)
         if (isValid)
-            res.send("Success")
+            createJwt(res, user)
         else
             res.send("not allowed")
     }
@@ -44,6 +54,35 @@ app.post('/users/login', async (req, res) => {
         res.status(500).send()
     }
 })
+
+function createJwt(res, user) {
+
+    const userId = { name: user.name }
+
+    const accessToken = jwt.sign(userId, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
+}
+
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username == req.user.name))
+})
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null)
+        return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)
 
